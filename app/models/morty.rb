c8458439a45c27a66c37d20d7f201f7e14350703
rafaelrocha <rafaelmachadodarocha@gmail.com>
2020-08-25 11:56:32 -0300

    implementing controllers

--- app/models/morty.rb
+++ app/models/morty.rb
@@ -2,4 +2,5 @@ class Morty < ApplicationRecord
   belongs_to :seller_rick, class_name: 'Rick', foreign_key: 'rick_id'
   has_one :exchange
   has_one :buyer_rick, through: :exchange, source: :rick
+  has_one_attached :photo
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :mortys
-  get '/search', to: 'mortys#search'
+  resources :morties
+  get '/search', to: 'morties#search'
 end
