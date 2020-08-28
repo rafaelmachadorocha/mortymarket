2952dffec65eed2585ef1270c26af9f0476f3c88
rafaelrocha <rafaelmachadodarocha@gmail.com>
2020-08-28 10:58:58 -0300

    fixing layout

--- app/javascript/channels/show_purchase_form.js
+++ app/javascript/channels/show_purchase_form.js
@@ -1,15 +1,20 @@
 const showPurchase = () => {
   const btn = document.getElementById('button-purchase');
   const card = document.getElementById('purchase-card');
-  btn.addEventListener('click', (event) => {
-    event.preventDefault();
-    card.classList.remove('display_none')
-    window.scrollTo({
-      top: (card.offsetTop + card.offsetHeight),
-      behavior: 'smooth'
-    });  
-  });
-
+  const msg = document.getElementById('purchase-stimulus');
+  if (btn) {
+    btn.addEventListener('click', (event) => {
+      event.preventDefault();
+      btn.setAttribute('disabled', 'true');
+      msg.classList.remove('display_none');
+      card.classList.remove('display_none');
+      window.scrollTo({
+        top: (card.offsetTop + card.offsetHeight),
+        behavior: 'smooth'
+      });  
+    });
+  }
 };
+
 
 export { showPurchase };
